7f36ad377b79e35eb1ff9b897d5e5492dbf05313
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-08-01 18:28:46 -0400

    working on put function

--- netflix_and_grill_app/app/assets/javascripts/application.js
+++ netflix_and_grill_app/app/assets/javascripts/application.js
@@ -24,15 +24,17 @@ const renderGrill = ( grill ) => {
   $container.append( $grill );
 }
 
-const getGrills = (e) => {
-  e.preventDefault()
-  let food = e.target.meats.value
+const getGrills = () => {
+  // e.preventDefault()
+  let food = "steak"
+  // let food = e.target.meats.value
   $.getJSON('/grills', {food}).done(( grills ) => {
     console.log(grills)
     let i = Math.floor(Math.random()*grills.matches.length)
-    let grill = grills.matches[i]
+    meat = grills.matches[i].imageUrlsBySize[90]
+    console.log(meat)
     // grills.matches.forEach(( grill ) => {
-      renderGrill( grill );
+      // renderGrill( grill );
     // })
   })
 }
@@ -43,7 +45,7 @@ const renderShows = ( show ) => {
   let $newRow = $('<div class="row">')
   let $lastRow =$('.row').eq(-1)
   let $form = $(`<form data-url="/bbqueue/new">`);
-  let $show = $( '<div class="five columns">' )
+  let $show = $( '<div class="three columns">' )
   let $img = $( '<img>' ).attr( 'src', show.artwork_208x117 )
   let $save = $('<input type="submit" name="save" value="save">')
   $show.append($form.append($img,$save))
@@ -54,7 +56,7 @@ const renderShows = ( show ) => {
 let counter = 0
 const getShows = (e) => {
   e.preventDefault()
-
+  $('#featured').text("Featured Programming")
   let choice = e.target.title.value
   $( '#show-list' ).empty()
   counter = 0;
@@ -63,14 +65,17 @@ const getShows = (e) => {
     shows.results.forEach(( show ) => {
       let $container = $( '#show-list' );
       let $newRow = $('<div class="row">')
-      if(counter % 2 === 0){
+      if(counter % 4 === 0){
         $container.append($newRow);
       }
       renderShows( show );
       counter++
+      $('#searchBar').attr('type', "hidden");
+      $('#search').show()
       // console.log(counter)
     })
   })
+  e.target.reset()
 }
 
 const getBBQ = (e) => {
@@ -82,38 +87,75 @@ const getBBQ = (e) => {
     shows.forEach(( show ) => {
       let $container = $( '#show-list' );
       let $newRow = $('<div class="row">')
-      if(counter % 2 === 0){
+      if(counter % 4 === 0){
         $container.append($newRow);
       }
       let $lastRow =$('.row').eq(-1)
-      let $show = $( '<div class="five columns">' )
-      let $img = $( '<img>' ).attr( 'src', show.image ).attr('display','block')
+      let $show = $( '<div class="three columns">' )
+      let $img = $( '<img>' ).attr( 'src', show.image ).attr('display','block').attr('data-url',`/bbqueue/${show.id}`).val(show.meat).attr('class', 'img')
       // let $pair = $('<input type="submit" name="getGrill" value="Pair">')
       let $delete = $(`<input type="submit" value="Binge Watched" class="delete">`).attr('data-url',`/bbqueue/${show.id}`)
       $show.append($img).append($delete)
       $lastRow.append( $show );
       $('.delete').click(deleteItem);
       counter++
-    })
   })
+      $('.img').on('mouseover', function(e){
+        let meat = $(this).val()
+        let img = $(this).attr('src')
+        $(this).attr('src',meat)
+        $(this).val(img)
+        console.log('on')
+      })
+      $('.img').on('mouseleave', function(e){
+        let meat = $(this).val()
+        let img = $(this).attr('src')
+        $(this).attr('src',meat)
+        $(this).val(img)
+        // console.log('off')
+      })
+      $('.img').on('click', function(e){
+        // let meat = ''
+        getGrills()
+        let url = $(this).attr('data-url')
+        let data = {meat:meat};
+        setTimeout(function(){
+        console.log(data)
+          $.ajax({
+            url: url,
+            method: 'put',
+            data: data
+          })
+        }, 1300)
+        setTimeout(function(){
+          getBBQ(e)
+        },1500)
+        // console.log('off')
+      })
+})
 }
 
-
+let meat = ""
 const createBBQ=(e)=>{
   e.preventDefault();
-console.log('hi')
+  getGrills()
   let $children = $(e.target).children();
-  $children.eq(1).val('Added to BBQueue')
+  $children.eq(1).val('Added to BBQueue').attr('class','delete')
+  setTimeout(function(){
   let data =  {
-                title    : "dummy",
+                title    : "fraiser",
                 image    : $children.eq(0).attr('src'),
-                meat     : "dummy"
+                meat     : meat
               }
   $.post('/bbqueue',data)
   .done(function(){
-    console.log(arguments);
-    })
+    console.log(arguments[0].id);
+    // let show = arguments[0].id
+    // $(e.target).attr('data-url',`/bbqueue/${show}`)
+    })}, 1000)
+  $('.delete').click(deleteItem);
 }
+
 
 const deleteItem = (e) =>{
   let url = $(e.target).attr('data-url');
@@ -127,8 +169,21 @@ const deleteItem = (e) =>{
   })
 }
 
-$(function() {
+const showSearch = (e) =>{
+  e.preventDefault()
+  $('#searchBar').attr('type', "text");
+  $('#search').hide()
+}
+
+
+$(() => {
   $('#grillin').submit(getGrills);
   $('#watchin').submit(getShows);
   $('#bbq').submit(getBBQ);
+  $('#search').submit(showSearch)
+  $('img').on('click', function(e){
+    let meat = $(this).val()
+    $(this).attr('src',meat)
+    console.log('click')
+  })
 })
